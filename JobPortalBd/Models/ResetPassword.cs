abb85c20cf12c2d02d54b51f5a282072fd2894d5
mdtoriqulislam08 <mdtoriqulislam08@gmail.com>
2018-12-07 12:57:33 +0600

    V.4  (4-12-18)

---
+++ JobPortalBd/Models/ResetPassword.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+namespace Jobprotal.Models
+{
+    public class ResetPasword
+    {
+        [Required(ErrorMessage = "Please enter password")]
+        [DataType(DataType.Password)]
+        [MinLength(6, ErrorMessage = "Minimum 6 characters required")]
+        public string NewPassword { get; set; }
+        [DataType(DataType.Password)]
+        [Compare("NewPassword", ErrorMessage = "Do not match the password")]
+        public string ConfirmPassword { get; set; }
+        [Required]
+        public string ResetCode { get; set; }
+    }
+}
+
